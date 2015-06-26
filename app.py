01e6aa083f8b74cd45b5dbac11a9318fa3f8cda2
jiaying.lu <lujiaying93@foxmail.com>
2015-06-26 19:58:41 +0800

    compelete init gmm

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ __all__ = ['app']
 
 import os
 import json
-from flask import Flask
+from flask import Flask, request
 import datetime
 import dao
 from config import *
@@ -51,10 +51,66 @@ def init_before_first_request():
     logger.info(init_tag + log_bugsnag_token)
     logger.info(init_tag + log_logentries_token)
 
-@app.route('/test/', methods=['GET'])
+@app.route('/tests/', methods=['GET'])
 def handle_test():
     return 'haha, YES'
 
 @app.route('/config/', methods=['GET'])
 def get_config():
     return json.dumps(dao.query_config())
+
+@app.route('/gmm/create/', methods=['POST'])
+def create_init_gmm():
+    '''create data in table `gmm` then init the params
+
+    Parameters
+    ----------
+    data: JSON Obj
+      e.g. {"tag":"init" , "covariance_value":1.2}
+      tag: string
+        tag of gmm models
+      convariance_value: float
+        convariance of gmm models
+
+    Returns
+    -------
+    code: int
+      0 success, 1 fail
+    message: string
+    result:
+    '''
+    if request.headers.has_key('X-Request-Id') and request.headers['X-Request-Id']:
+        x_request_id = request.headers['X-Request-Id']
+    else:
+        x_request_id = ''
+
+    logger.info('<%s>, [create gmm] enter' %(x_request_id))
+    result = {'code': 1, 'message': ''}
+
+    # params JSON validate
+    try:
+        incoming_data = json.loads(request.data)
+    except ValueError, err_msg:
+        logger.exception('<%s>, [ValueError] err_msg: %s, params=%s' % (x_request_id, err_msg, request.data))
+        result['message'] = 'Unvalid params: NOT a JSON Object'
+        return json.dumps(result)
+
+    # params key checking
+    for key in ['tag', 'covariance_value']:
+        if key not in incoming_data:
+            logger.exception("<%s>, [KeyError] params=%s, should have key: %s" % (x_request_id, incoming_data, key))
+            result['message'] = "Params content Error: cant't find key=%s" % (key)
+            return json.dumps(result)
+
+    logger.info('<%s>, [create gmm] valid request with params=%s' %(x_request_id, incoming_data))
+
+    # init gmm models
+    n_components = 24
+    means_ = [i*3600*1000 for i in xrange(24)]
+    poi_configs = dao.query_config()
+    covars_ = [incoming_data['covariance_value']] * n_components
+
+    dao.save_init_gmm(incoming_data['tag'], poi_configs.keys(), n_components, covars_, means_)
+
+    result = {'code': 0, 'message': 'success'}
+    return json.dumps(result)
--- dao.py
+++ dao.py
@@ -5,7 +5,8 @@
 __author__ = 'jiaying.lu'
 __all__ = ['query_config']
 
-from leancloud import Object, Query
+from leancloud import Query, Object
+import datetime
 
 def query_config(config_name='pois_type'):
     '''
@@ -27,3 +28,47 @@ def query_config(config_name='pois_type'):
     return poi_configs
 
 
+def save_init_gmm(tag, event_labels, n_components, covars_, means_):
+    '''
+    save init params of gmm, init num is accroding to len(event_labels)
+
+    Parameters
+    ----------
+    tag: string
+      gmm model's tag
+    event_labels: list of string
+      gmm model's label
+    n_components: int
+    covars_: list of float
+      len(means_) == n_components
+    means_: list of float
+      len(means_) == n_components
+
+    Returns
+    -------
+    flag: boolean
+    '''
+    GMM = Object.extend('gmm')
+    description = 'init save using default params'
+    covariance_type = 'full'
+    gmm_params = {
+        'nMix': n_components,
+        'covarianceType': covariance_type,
+        'means': means_,
+        'covars': covars_
+    }
+    count = 0
+
+    for event_label in event_labels:
+        gmm = GMM()
+        gmm.set('description', description)
+        gmm.set('eventLabel', event_label)
+        gmm.set('params', gmm_params)
+        gmm.set('count', count)
+        gmm.set('covarianceType', covariance_type)
+        gmm.set('tag', tag)
+        gmm.set('nMix', n_components)
+        gmm.set('timestamp', datetime.datetime.now())
+        gmm.save()
+
+    return True
